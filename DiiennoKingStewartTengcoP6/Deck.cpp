9fea1c2b6fa4d87f7d25cdf20b5222a81d8980e1
donadthenomad <73919806+donadthenomad@users.noreply.github.com>
2020-11-03 19:41:06 -0700

    Update Deck.cpp

--- DiiennoKingStewartTengcoP6/Deck.cpp
+++ DiiennoKingStewartTengcoP6/Deck.cpp
@@ -2,17 +2,45 @@
 
 Deck::Deck()
 {
+	//seed the random_engine object
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::shuffle(this->cards.begin(), this->cards.end(), std::default_random_engine(seed));
+    //initialize the cards from 0 to 51
+    for (int i = 0; i < 51; i++) {
+        cards[i];
+    }
+    //shuffle
+    Shuffle();
 }
 
 void Deck::Shuffle()
 {
+	for (int i = 0; i < (52 - 1); i++) {
+        int j = i + (rand() % (52 - i));
+        swap(cards[i], cards[j]);
+
+        //if both cards are the same reshuffle again
+        if (i == j) {
+            Shuffle(); 
+        }
+	}
 }
 
 int Deck::RandomCard()
 {
+	//returns a random int using uniform distributuion
+    //needs more work
+    uniform_int_distribution<int> distribution(0, 51);
 	return 0;
 }
 
 void Deck::Deal(Card& c)
 {
+	 Card deal;
+    deal = cards[topCard];
+    topCard++;
+    if (topCard <= 34) {
+        Shuffle();
+        topCard = 0;
+    }
 }
